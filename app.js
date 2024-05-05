cda07599b9eefed6fcf0bf95a033617b12aaab84
Barış Dilekçi <barisdilekci@Bars-MacBook-Air.local>
2024-05-06 02:38:02 +0300

    feat: create middlewares

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+// Form verilerini işlemek için express'in yerleşik middleware'ini kullanın
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+    res.send("Form submitted successfully!"); // Yanıt gönder
+});
+
+app.listen(port, () => {
+    console.log(`Listening port ${port}`);
+});
